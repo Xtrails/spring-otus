c88b3ebd9d2bff7ca7e30f702f8efcc16ea5963a
panisimov <anisimov.prod@gmail.com>
2019-11-27 15:23:39 +0400

    lesson02 - anotation configuration + bundle localization

--- src/main/java/ru/otus/spring01/config/AppConfig.java
+++ src/main/java/ru/otus/spring01/config/AppConfig.java
@@ -1,18 +1,12 @@
 package ru.otus.spring01.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 
 @Configuration
-@PropertySource("application.properties")
 public class AppConfig {
-
-    @Value("${bundle.path}")
-    private String bundlePath;
 
     @Bean
     public MessageSource messageSource(){
